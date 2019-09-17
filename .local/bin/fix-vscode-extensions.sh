bf115b9151e5994d3da0c9ce717641891b5b6c3d
Dennis S. H. Andersen <dennis@dennisandersen.dk>
2019-09-17 20:11:49 +0200

    ...

--- .local/bin/fix-vscode-extensions.sh
+++ .local/bin/fix-vscode-extensions.sh
@@ -15,8 +15,6 @@ code --install-extension ms-azuretools.vscode-docker --force
 code --install-extension ms-python.python --force
 code --install-extension ms-vscode.cpptools --force
 code --install-extension ms-vscode.sublime-keybindings --force
-code --install-extension vsciot-vscode.vscode-arduino --force
 code --install-extension wayou.vscode-todo-highlight --force
 code --install-extension platformio.platformio-ide --force
 code --install-extension vscode-yaml --force
-
