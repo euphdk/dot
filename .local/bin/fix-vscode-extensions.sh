3202aa206c3662da38cc43876eb133a64029f508
Dennis S. H. Andersen <dennis@dennisandersen.dk>
2019-09-17 19:35:18 +0200

    Add --force to upgrade

--- .local/bin/fix-vscode-extensions.sh
+++ .local/bin/fix-vscode-extensions.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-code --install-extension CoenraadS.bracket-pair-colorizer-2
-code --install-extension dave-hagedorn.jenkins-runner
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension donjayamanne.githistory
-code --install-extension DotJoshJohnson.xml
-code --install-extension EditorConfig.EditorConfig
-code --install-extension janjoerke.jenkins-pipeline-linter-connector
-code --install-extension johnstoncode.svn-scm
-code --install-extension marlon407.code-groovy
-code --install-extension mikestead.dotenv
-code --install-extension mrmlnc.vscode-apache
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.sublime-keybindings
-code --install-extension vsciot-vscode.vscode-arduino
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension platformio.platformio-ide
-code --install-extension vscode-yaml
+code --install-extension --force CoenraadS.bracket-pair-colorizer-2
+code --install-extension --force dave-hagedorn.jenkins-runner
+code --install-extension --force DavidAnson.vscode-markdownlint
+code --install-extension --force donjayamanne.githistory
+code --install-extension --force DotJoshJohnson.xml
+code --install-extension --force EditorConfig.EditorConfig
+code --install-extension --force janjoerke.jenkins-pipeline-linter-connector
+code --install-extension --force johnstoncode.svn-scm
+code --install-extension --force marlon407.code-groovy
+code --install-extension --force mikestead.dotenv
+code --install-extension --force mrmlnc.vscode-apache
+code --install-extension --force ms-azuretools.vscode-docker
+code --install-extension --force ms-python.python
+code --install-extension --force ms-vscode.cpptools
+code --install-extension --force ms-vscode.sublime-keybindings
+code --install-extension --force vsciot-vscode.vscode-arduino
+code --install-extension --force wayou.vscode-todo-highlight
+code --install-extension --force platformio.platformio-ide
+code --install-extension --force vscode-yaml
 
